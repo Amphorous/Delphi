d03f154ccd9418518af49dac9b3abc6ae4402be5
Asim <161995961+MdMusthaqAsim@users.noreply.github.com>
2026-04-04 20:47:38 +0530

    first push master

---
+++ src/test/java/org/hoyo/celestiaeureka/CelestiaEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hoyo.celestiaeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelestiaEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
